092fda39b9c5cf6a477ca662f4a55e9a1bba6f11
joshuakwhiddon <66791995+joshuakwhiddon@users.noreply.github.com>
2020-07-08 19:52:06 -0700

    Add files via upload
    just create a password.txt file in the same place you store the code

---
+++ password generator.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 12:39:20 2020
+
+@author: whidd
+"""
+#packages
+import random
+#Files
+file = open('password.txt', 'a')
+#variables
+website = input('enter name of website for password:')
+username = input('enter your username:')
+number = input('How long do you want you password:')
+number = int(number)
+#password key
+options = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',':',';','<','>',',','.','/','?','[',']','!','@','#','$','%','^','&','*','(',')','-','=','`','~' ]
+#randomization code
+answer = random.SystemRandom()
+CODE = ""
+
+#generator
+while number > 0:
+    CODE = CODE + answer.choice(options)
+    number = number - 1
+
+#output
+if number <= 0:
+    print(CODE)
+    file.write('------------- \n' + 'website url: ' + website + '\n')
+    file.write('\n' + 'Username: ' + username + '\n')
+    file.write('Password:' + CODE + '\n------------- \n')
+    file.close()
+else:
+    print('void')
