c69b9ddadcd9aa6a1282e52fd827f134f2c2d4f3
joshuakwhiddon <66791995+joshuakwhiddon@users.noreply.github.com>
2020-08-13 09:30:43 -0500

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,30 +1,39 @@
-
 #packages
 import random
-#Files
+#File
 file = open('password.txt', 'a')
-#variables
-website = input('enter name of website for password:')
-username = input('enter your username:')
-number = input('How long do you want you password:')
-number = int(number)
-#password key
-options = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',':',';','<','>',',','.','/','?','[',']','!','@','#','$','%','^','&','*','(',')','-','=','`','~' ]
-#randomization code
-answer = random.SystemRandom()
-CODE = ""
+global number, CODE, answer, options, website, username
+def first():
+  global number, CODE , answer, options, website, username
+  print('hi')
+  website = input('enter name of website for password:')
+  username = input('enter your username:')
+  number = input('How long do you want you password:')
+  number = int(number)
+  second()
 
-#generator
-while number > 0:
+def second():
+  global number, CODE, answer, options, website, username
+  options = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',':',';','<','>',',','.','/','?','[',']','!','@','#','$','%','^','&','*','(',')','-','=','`','~' ]
+  #randomise
+  answer = random.SystemRandom()
+  CODE = ""
+  gen()
+
+def gen():
+  global number, CODE, answer, options, website, username
+  while number > 0:
     CODE = CODE + answer.choice(options)
     number = number - 1
-
-#output
-if number <= 0:
+  if number <= 0:
     print(CODE)
     file.write('------------- \n' + 'website url: ' + website + '\n')
     file.write('\n' + 'Username: ' + username + '\n')
     file.write('Password:' + CODE + '\n------------- \n')
     file.close()
-else:
-    print('void')
+  else:
+    print('void')  
+
+
+
+first()
